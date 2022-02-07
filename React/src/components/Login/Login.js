9a79792e9b39e8db4592bee5c1e29d998dd392ee
Jonathan-Lorfils <jonathan.11911@gmail.com>
2022-02-07 13:43:03 -0500

    commit react

---
+++ React/src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Login = () => {
+  
+  const userLogin = async (email,password) =>{
+      
+  }
+  
+  
+  
+  
+    return (
+    <div>
+
+    </div>
+    );
+};
+
+export default Login;
